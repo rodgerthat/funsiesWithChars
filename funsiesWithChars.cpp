6c5bca73c06ff000adeb0c8b86e8206e9e2d5683
joelrnorris <joel.r.norris@gmail.com>
2017-03-16 13:02:21 -0500

    initial commit

---
+++ funsiesWithChars.cpp
@@ -0,0 +1,14 @@
+// funsiesWithChars.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+int main()
+{
+
+	char a = 0xab;
+	std::cout << +a;
+
+    return 0;
+}
+
